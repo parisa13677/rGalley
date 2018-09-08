91253a741d54a8fd5dc0a50b6690917a6a7acc2a
parisa <parisa.golanbary@gmail.com>
2018-09-08 18:49:26 +0430

    add methods new,create,image_params, to upload image 7 connect to view.

--- app/controllers/images_controller.rb
+++ app/controllers/images_controller.rb
@@ -3,11 +3,28 @@ class ImagesController < ApplicationController
   end
 
   def new
+    @image = Image.all
   end
 
   def create
+    @image = Image.new(image_params)
+
+    if @image.save
+      flash[:notice] = 'Image Created'
+
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   def destroy
+  end
+
+  private
+
+  def image_params
+    params.require(:image).permit(:image, :category_id, :image_title,
+      :image_descrip, :image_file_size, :image_content_type, :remote_image_url)
   end
 end
