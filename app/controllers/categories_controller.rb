36e31ab7d1ba79bb6ce7d207eff8c586fcb1258f
parisa <parisa.golanbary@gmail.com>
2018-09-08 17:50:42 +0430

    add method new,create,category_params to connect view for add category.

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,32 @@
+# frozen_string_literal: true
+
+# this is a CategoriesController
 class CategoriesController < ApplicationController
   def new
+    @category = Category.new
   end
 
   def edit
   end
 
   def create
+    @category = Category.new(category_params)
+
+    if @category.save
+      flash[:notice] = "Category Created"
+
+      redirect_to root_path
+    else
+      render 'new'
+    end
   end
 
   def update
+  end
+
+private
+
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
