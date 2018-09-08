686abdc855286fe6247810cf7f765b751bbeeb87
parisa <parisa.golanbary@gmail.com>
2018-09-08 13:41:23 +0430

    connect to image and category model with code in category :has_many :images then in image add :belongs_to :category .

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,2 +1,5 @@
+# frozen_string_literal: true
+
+# this is a model category
 class Category < ApplicationRecord
 end
--- app/models/image.rb
+++ app/models/image.rb
@@ -1,2 +1,6 @@
+# frozen_string_literal: true
+
+# this is a model image
 class Image < ApplicationRecord
+    belongs_to :category
 end
