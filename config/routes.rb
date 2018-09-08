1c62960b4579fb0c28a36eda91579cc15f28f881
parisa <parisa.golanbary@gmail.com>
2018-09-08 12:40:04 +0430

    create controller categories & images with category model then create all of views and added to routs file then migrate it.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
+  get 'images/index'
+  get 'images/new'
+  get 'images/create'
+  get 'images/destroy'
+  get 'categories/new'
+  get 'categories/edit'
+  get 'categories/create'
+  get 'categories/update'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
