5a4e3e9c4ea12640acadc32532492cd9cc499407
parisa <parisa.golanbary@gmail.com>
2018-09-08 23:49:31 +0430

    add comment to all file.

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
+# frozen_string_literal: true
+
 Rails.application.routes.draw do
-  get "upload" => "images#new", :as => "upload"
+  get 'upload' => 'images#new', :as => 'upload'
   get 'images/index'
   get 'images/new'
   get 'images/create'
