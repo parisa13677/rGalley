6e0d44f48bf6cb7418467955f65cb4fc55f213f8
parisa <parisa.golanbary@gmail.com>
2018-09-08 16:25:16 +0430

    add getcategory method for known @categoryNav.

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,13 @@
+# frozen_string_literal: true
+
+# this is a ApplicationController
 class ApplicationController < ActionController::Base
+  before_action :getcategoryNav
+
+  protect_from_forgery with: :exception
+
+  private
+  def getcategoryNav
+    @categoryNav = Category.all
+  end
 end
